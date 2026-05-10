f51acd1b9878d27d5db6153ae7125e4efd272c20
Rushikesh Sangar <148414253+rushikesh-sangar@users.noreply.github.com>
2026-05-10 19:01:39 +0530

    Add files via upload

---
+++ Flask implementation/Flask introduction.py
@@ -0,0 +1,12 @@
+# Flask introduction
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Welcome to flask'
+
+# app.run(host = '0.0.0.0', port = 81)
+app.run(host = '127.0.0.1', port = 81)
